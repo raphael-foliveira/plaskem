af1aa4f751bec5f571c68e03d1beac70142c8389
raphael <raphael.foliveira1@gmail.com>
2022-10-30 01:53:42 -0300

    Remove comments

--- components/HomeComponents/HomeContactForm.tsx
+++ components/HomeComponents/HomeContactForm.tsx
@@ -14,23 +14,9 @@ import {
 export default function HomeContactForm() {
     return (
         <Flex backgroundColor="#f9fafb" width={"100%"} wrap="wrap" paddingTop="5em">
-            <Box width={{ base: "10%", md: "40%" }}></Box>
-            <Box width={{ base: "80%", md: "50%" }}></Box>
-            {/* <Box width={{ base: "10%", md: "20%" }}></Box> */}
-            {/* <Stack textAlign={"end"} width="30%" lineHeight={"2"} paddingRight={"2em"}>
-                <label htmlFor="name">Nome</label>
-                <label htmlFor="email">Email</label>
-                <label htmlFor="phone">DDD + Telefone</label>
-                <label htmlFor="message">Mensagem</label>
-            </Stack> */}
-            <Stack
-                textAlign={"end"}
-                width={{ base: "100%", lg: "67%" }}
-                gap={3}
-                padding="1.5em"
-                // justifyContent="space-between"
-            >
-                <Box textAlign={"left"} marginLeft={{lg:"50%"}}>
+            <Box width={{ base: "10%", md: "40%" }} />
+            <Stack textAlign={"end"} width={{ base: "100%", lg: "67%" }} gap={3} padding="1.5em">
+                <Box textAlign={"left"} marginLeft={{ lg: "50%" }}>
                     <Heading marginBottom={4}>Entre em contato com a gente</Heading>
                     <Text marginBottom={6}>
                         Para saber mais sobre os nossos produtos, deixe uma mensagem que entraremos
