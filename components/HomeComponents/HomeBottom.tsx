bf35c219beb8ee805601ca02db8c8aba6bb13ca0
raphael <raphael.foliveira1@gmail.com>
2022-10-31 22:19:33 -0300

    Swtich <a> for <Link/> in footer links

--- components/HomeComponents/HomeBottom.tsx
+++ components/HomeComponents/HomeBottom.tsx
@@ -1,4 +1,5 @@
 import { Box, Flex, Text } from "@chakra-ui/react";
+import Link from "next/link";
 import { PlaskemLogo } from "../UI/Assets";
 
 export default function HomeBottom() {
@@ -13,7 +14,12 @@ export default function HomeBottom() {
                     <a>(71) 3186-0399</a>
                 </Text>
             </Box>
-            <Flex width={{ base: "100%", lg: "30%" }} justifyContent="center" alignItems={"center"} padding="3em">
+            <Flex
+                width={{ base: "100%", lg: "30%" }}
+                justifyContent="center"
+                alignItems={"center"}
+                padding="3em"
+            >
                 <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3889.070125752584!2d-38.3373800846589!3d-12.903212290900761!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x716163f267fa54d%3A0x82da6a19b652748f!2sPlaskem%20Embalagens!5e0!3m2!1sen!2sbr!4v1667103629110!5m2!1sen!2sbr"
                     width="600"
@@ -21,32 +27,37 @@ export default function HomeBottom() {
                     loading="lazy"
                 ></iframe>
             </Flex>
-            <Box width={{ base: "100%", lg: "20%" }} textAlign="center" padding="3em" lineHeight={"2em"}> 
+            <Box
+                width={{ base: "100%", lg: "20%" }}
+                textAlign="center"
+                padding="3em"
+                lineHeight={"2em"}
+            >
                 <Text>
-                    <a href="/empresa">Sobre nós</a>
+                    <Link href="/empresa">Sobre nós</Link>
                 </Text>
                 <Text>
-                    <a href="/politica-de-qualidade">Política de qualidade</a>
+                    <Link href="/politica-de-qualidade">Política de qualidade</Link>
                 </Text>
                 <Text>
-                    <a href="/produtos">Nossos produtos</a>
+                    <Link href="/produtos">Nossos produtos</Link>
                 </Text>
                 <Text>
-                    <a href="/meio-ambiente">Meio ambiente</a>
-                </Text>
-            </Box>
-            <Box width={{ base: "100%", lg: "20%" }} textAlign="center" padding="3em" lineHeight={"2em"}>
-                <Text>
-                    <a>Trabalhe conosco</a>
-                </Text>
-                <Text>
-                    <a href="/fale-conosco">Contato</a>
-                </Text>
-                <Text>
-                    <a>Pesquisa de satisfação</a>
+                    <Link href="/meio-ambiente">Meio ambiente</Link>
                 </Text>
             </Box>
-            
+            <Box
+                width={{ base: "100%", lg: "20%" }}
+                textAlign="center"
+                padding="3em"
+                lineHeight={"2em"}
+            >
+                <Text>Trabalhe conosco</Text>
+                <Text>
+                    <Link href="/fale-conosco">Contato</Link>
+                </Text>
+                <Text>Pesquisa de satisfação</Text>
+            </Box>
         </Flex>
     );
 }
