8d276209f23a99d09e6851bf7ea12d9f30aaec44
raphael <raphael.foliveira1@gmail.com>
2022-11-03 19:19:00 -0300

    Increase gap between form fields

--- components/Forms/ContactForm.tsx
+++ components/Forms/ContactForm.tsx
@@ -16,12 +16,12 @@ import { FormEvent } from "react";
 export default function ContactForm() {
     const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-    }
+    };
 
     return (
         <>
             <Box width={{ base: "10%", md: "40%" }} />
-            <Stack textAlign={"end"} width={{ base: "100%" }} gap={3} padding="1.5em">
+            <Stack textAlign={"end"} width={{ base: "100%" }} padding="1.5em">
                 <Box textAlign={"left"} marginLeft={{ lg: "30%" }} width={{ lg: "40%" }}>
                     <Heading marginBottom={4}>Entre em contato com a gente</Heading>
                     <Text marginBottom={6}>
@@ -30,47 +30,49 @@ export default function ContactForm() {
                     </Text>
                 </Box>
                 <form action="" onSubmit={handleSubmit}>
-                    <FormControl isRequired>
-                        <InputGroup>
-                            <Flex width={{ base: "25%", lg: "30%" }} justifyContent="flex-end">
-                                <FormLabel>Nome</FormLabel>
-                            </Flex>
-                            <Input name="name" width={{ base: "75%", lg: "40%" }} />
-                        </InputGroup>
-                    </FormControl>
-                    <FormControl isRequired>
-                        <InputGroup>
-                            <Flex width={{ base: "25%", lg: "30%" }} justifyContent="flex-end">
-                                <FormLabel>Email</FormLabel>
-                            </Flex>
-                            <Input name="email" width={{ base: "75%", lg: "40%" }} />
-                        </InputGroup>
-                    </FormControl>
-                    <FormControl isRequired>
-                        <InputGroup>
-                            <Flex width={{ base: "25%", lg: "30%" }} justifyContent="flex-end">
-                                <FormLabel>DDD + Telefone</FormLabel>
-                            </Flex>
-                            <Input name="phone" width={{ base: "75%", lg: "40%" }} />
-                        </InputGroup>
-                    </FormControl>
-                    <FormControl isRequired>
-                        <InputGroup>
-                            <Flex width={{ base: "25%", lg: "30%" }} justifyContent="flex-end">
-                                <FormLabel>Mensagem</FormLabel>
-                            </Flex>
-                            <Textarea
-                                rows={5}
-                                name="message"
-                                resize={"none"}
-                                width={{ base: "75%", lg: "40%" }}
-                            />
-                        </InputGroup>
-                    </FormControl>
+                    <Stack gap={3}>
+                        <FormControl isRequired>
+                            <InputGroup>
+                                <Flex width={{ base: "25%", lg: "30%" }} justifyContent="flex-end">
+                                    <FormLabel>Nome</FormLabel>
+                                </Flex>
+                                <Input name="name" width={{ base: "75%", lg: "40%" }} />
+                            </InputGroup>
+                        </FormControl>
+                        <FormControl isRequired>
+                            <InputGroup>
+                                <Flex width={{ base: "25%", lg: "30%" }} justifyContent="flex-end">
+                                    <FormLabel>Email</FormLabel>
+                                </Flex>
+                                <Input name="email" width={{ base: "75%", lg: "40%" }} />
+                            </InputGroup>
+                        </FormControl>
+                        <FormControl isRequired>
+                            <InputGroup>
+                                <Flex width={{ base: "25%", lg: "30%" }} justifyContent="flex-end">
+                                    <FormLabel>DDD + Telefone</FormLabel>
+                                </Flex>
+                                <Input name="phone" width={{ base: "75%", lg: "40%" }} />
+                            </InputGroup>
+                        </FormControl>
+                        <FormControl isRequired>
+                            <InputGroup>
+                                <Flex width={{ base: "25%", lg: "30%" }} justifyContent="flex-end">
+                                    <FormLabel>Mensagem</FormLabel>
+                                </Flex>
+                                <Textarea
+                                    rows={5}
+                                    name="message"
+                                    resize={"none"}
+                                    width={{ base: "75%", lg: "40%" }}
+                                />
+                            </InputGroup>
+                        </FormControl>
 
-                    <Flex justifyContent={"flex-end"} width={{ lg: "70%" }}>
-                        <Button type="submit">Enviar</Button>
-                    </Flex>
+                        <Flex justifyContent={"flex-end"} width={{ lg: "70%" }}>
+                            <Button type="submit">Enviar</Button>
+                        </Flex>
+                    </Stack>
                 </form>
             </Stack>
             <Box width={{ base: "0", lg: "25%" }}></Box>
--- components/Forms/PesquisaDeSatisfacaoForm.tsx
+++ components/Forms/PesquisaDeSatisfacaoForm.tsx
@@ -17,8 +17,8 @@ import { FormEvent } from "react";
 export default function PesquisaDeSatisfacaoForm() {
     const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-    }
-    
+    };
+
     return (
         <>
             <Box width={{ base: "10%", md: "40%" }} />
@@ -32,64 +32,66 @@ export default function PesquisaDeSatisfacaoForm() {
                     </Text>
                 </Box>
                 <form action="" onSubmit={handleSubmit}>
-                    <FormControl isRequired>
-                        <InputGroup>
-                            <Flex width={{ base: "25%", lg: "30%" }} justifyContent="flex-end">
-                                <FormLabel>Nome</FormLabel>
-                            </Flex>
-                            <Input name="name" width={{ base: "75%", lg: "40%" }} />
-                        </InputGroup>
-                    </FormControl>
-                    <FormControl isRequired>
-                        <InputGroup>
-                            <Flex width={{ base: "25%", lg: "30%" }} justifyContent="flex-end">
-                                <FormLabel>Email</FormLabel>
-                            </Flex>
-                            <Input name="email" width={{ base: "75%", lg: "40%" }} />
-                        </InputGroup>
-                    </FormControl>
-                    <FormControl isRequired>
-                        <InputGroup>
-                            <Flex width={{ base: "25%", lg: "30%" }} justifyContent="flex-end">
-                                <FormLabel>DDD + Telefone</FormLabel>
-                            </Flex>
-                            <Input name="phone" width={{ base: "75%", lg: "40%" }} />
-                        </InputGroup>
-                    </FormControl>
-                    <FormControl isRequired>
-                        <InputGroup>
-                            <Flex width={{ base: "25%", lg: "30%" }} justifyContent="flex-end">
-                                <FormLabel>Nível de satisfação</FormLabel>
-                            </Flex>
-                            <Select
-                                name="evaluation"
-                                width={{ base: "75%", lg: "40%" }}
-                                placeholder="Selecione uma opção"
-                            >
-                                <option value="Muito satisfeito">Muito satisfeito</option>
-                                <option value="Satisfeito">Satisfeito</option>
-                                <option value="Bom">Bom</option>
-                                <option value="Regular">Regular</option>
-                                <option value="Ruim">Ruim</option>
-                            </Select>
-                        </InputGroup>
-                    </FormControl>
-                    <FormControl>
-                        <InputGroup>
-                            <Flex width={{ base: "25%", lg: "30%" }} justifyContent="flex-end">
-                                <FormLabel>Mensagem</FormLabel>
-                            </Flex>
-                            <Textarea
-                                rows={5}
-                                name="message"
-                                resize={"none"}
-                                width={{ base: "75%", lg: "40%" }}
-                            />
-                        </InputGroup>
-                    </FormControl>
-                    <Flex justifyContent={"flex-end"} width={{ lg: "70%" }}>
-                        <Button type="submit">Enviar</Button>
-                    </Flex>
+                    <Stack gap={3}>
+                        <FormControl isRequired>
+                            <InputGroup>
+                                <Flex width={{ base: "25%", lg: "30%" }} justifyContent="flex-end">
+                                    <FormLabel>Nome</FormLabel>
+                                </Flex>
+                                <Input name="name" width={{ base: "75%", lg: "40%" }} />
+                            </InputGroup>
+                        </FormControl>
+                        <FormControl isRequired>
+                            <InputGroup>
+                                <Flex width={{ base: "25%", lg: "30%" }} justifyContent="flex-end">
+                                    <FormLabel>Email</FormLabel>
+                                </Flex>
+                                <Input name="email" width={{ base: "75%", lg: "40%" }} />
+                            </InputGroup>
+                        </FormControl>
+                        <FormControl isRequired>
+                            <InputGroup>
+                                <Flex width={{ base: "25%", lg: "30%" }} justifyContent="flex-end">
+                                    <FormLabel>DDD + Telefone</FormLabel>
+                                </Flex>
+                                <Input name="phone" width={{ base: "75%", lg: "40%" }} />
+                            </InputGroup>
+                        </FormControl>
+                        <FormControl isRequired>
+                            <InputGroup>
+                                <Flex width={{ base: "25%", lg: "30%" }} justifyContent="flex-end">
+                                    <FormLabel>Nível de satisfação</FormLabel>
+                                </Flex>
+                                <Select
+                                    name="evaluation"
+                                    width={{ base: "75%", lg: "40%" }}
+                                    placeholder="Selecione uma opção"
+                                >
+                                    <option value="Muito satisfeito">Muito satisfeito</option>
+                                    <option value="Satisfeito">Satisfeito</option>
+                                    <option value="Bom">Bom</option>
+                                    <option value="Regular">Regular</option>
+                                    <option value="Ruim">Ruim</option>
+                                </Select>
+                            </InputGroup>
+                        </FormControl>
+                        <FormControl>
+                            <InputGroup>
+                                <Flex width={{ base: "25%", lg: "30%" }} justifyContent="flex-end">
+                                    <FormLabel>Mensagem</FormLabel>
+                                </Flex>
+                                <Textarea
+                                    rows={5}
+                                    name="message"
+                                    resize={"none"}
+                                    width={{ base: "75%", lg: "40%" }}
+                                />
+                            </InputGroup>
+                        </FormControl>
+                        <Flex justifyContent={"flex-end"} width={{ lg: "70%" }}>
+                            <Button type="submit">Enviar</Button>
+                        </Flex>
+                    </Stack>
                 </form>
             </Stack>
             <Box width={{ base: "0", lg: "25%" }}></Box>
